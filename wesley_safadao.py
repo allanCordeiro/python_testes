46d9013bee304fe966747ea76ce9c837bde3576f
AllanCordeiro <allan.cordeiro.santos@gmail.com>
2016-12-31 18:05:04 -0200

    subindo arquivo do wesley safadao

---
+++ wesley_safadao.py
@@ -0,0 +1,14 @@
+#vi isso em algum forum
+#algum professor de programacao passou este exercicio em alguma linguagem X
+#estou replicando-a em python
+
+#vagabundo=somatorio(mes)+(ano/100)*(50-dia)
+#anjo = 100 - safadeza
+
+dia = int(input("informe dia de nascimento: "))
+mes = int(input("informe mes de nascimento: "))
+ano = int(input("informe ano de nascimento: "))
+
+vagabundo = mes + (ano/100) * (50-dia)
+anjo = 100 - vagabundo
+print("Voce eh %d anjo, mas %d vagabundo" %(vagabundo, anjo))
